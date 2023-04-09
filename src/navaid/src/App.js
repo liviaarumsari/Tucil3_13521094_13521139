39b9d711d5129febbbdc47fd1764ad4fbd599b8c
nathania01 <nathania.calista01@gmail.com>
2023-04-09 20:11:42 +0700

    feat : UCS algorithm

--- src/navaid/src/App.js
+++ src/navaid/src/App.js
@@ -1,54 +1,12 @@
 import "./App.css";
 import React from "react";
 import { useState } from "react";
+import ucs from "./UCS";
 
-var prioqueue = [
-  {
-    nodes : [],
-    count : 0
-  }
-]
-
-function getLastElement(obj){
-  console.log(obj)
-  return obj.nodes[obj.nodes.length-1]
-}
-
-function newQueue(node,count){
-  prioqueue[0].nodes = node;
-  prioqueue[0].count = count;
-}
-
-function getNodes(adjMatrix){
-  let temp = prioqueue.shift();
-  let length = temp.nodes.length
-  let node = temp.nodes[length-1];
-  for(let i = 0; i < adjMatrix.length;i++){
-    if(parseInt(adjMatrix[node][i]) !== -1 && parseInt(adjMatrix[node][i])!== 0 && !temp.nodes.includes(i)){
-      let nodes = temp.nodes.concat([i])
-      let count = temp.count + parseInt(adjMatrix[node][i])
-      let newObj = {nodes,count}
-      prioqueue.push(newObj)
-    }
-  }
-  prioqueue.sort((a,b) => a.count - b.count)
-}
-
-function ucs(adjMatrix, startNode, targetNode) {
-  // Banyaknya nodes
-  const numNodes = adjMatrix.length;
-  let start = [startNode];
-  newQueue(start,0);
-  getNodes(adjMatrix);
-  while(getLastElement(prioqueue[0]) !== targetNode && prioqueue.length !== 0){
-    getNodes(adjMatrix)
-  }
-  console.log("Minimum path : ",prioqueue[0].count)
-  return -1; // Return -1 if no path found
-}
 function App() {
   
   const [fileContent, setFileContent] = useState(null);
+  
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -61,7 +19,13 @@ function App() {
         lines[i] = temp
       }      
       setFileContent(lines);
-      ucs(lines,0,4);
+      let answer = ucs(lines,0,1);
+      let routes = answer[0]
+      if(answer[1] === -1){
+        setFileContent("Tidak ada lintasan yang menhubungkan")
+      }else{
+        setFileContent("Jarak minum terdekat : " + answer[1] + "\nRute yang ditempuh : " + routes[0].nodes)
+      }
     };
     reader.readAsText(file);
   };
---
+++ src/navaid/src/UCS.js
@@ -0,0 +1,68 @@
+var prioqueue = [
+    {
+      nodes : [],
+      count : 0
+    }
+  ]
+  
+function getLastElement(obj){
+    return obj.nodes[obj.nodes.length-1]
+}
+
+function newQueue(node,count){
+    prioqueue[0].nodes = node;
+    prioqueue[0].count = count;
+}
+
+function searchNodes(nodes, count){
+    let node = nodes[nodes.length-1]
+    for(let i = 0; i < prioqueue.length;i++){
+        if(prioqueue[i].nodes.includes(node)){
+            if(count < prioqueue[i].count){
+                prioqueue.splice(i,1)
+                prioqueue.push({nodes,count})
+            }
+        }
+    }
+}
+
+function getNodes(adjMatrix,visited){
+    let temp = prioqueue.shift();
+    let length = temp.nodes.length
+    let node = temp.nodes[length-1];
+    for(let i = 0; i < adjMatrix.length;i++){
+        if(parseInt(adjMatrix[node][i]) !== -1 && parseInt(adjMatrix[node][i])!== 0 && !temp.nodes.includes(i)){
+            let nodes = temp.nodes.concat([i])
+            let count = temp.count + parseInt(adjMatrix[node][i])
+            if(!visited[i]){
+                let newObj = {nodes,count}
+                visited[i] = true
+                prioqueue.push(newObj)
+            }else{
+                // Kondisi kalau misalnya sudah divisit, pertama cari jalur mana saja yang mengarah ke nodes yg diinginkan
+                searchNodes(nodes,count)
+
+            }
+        
+        }
+    }
+    prioqueue.sort((a,b) => a.count - b.count)
+}
+
+export default function ucs(adjMatrix, startNode, targetNode) {
+    // Banyaknya nodes
+    const numNodes = adjMatrix.length;
+    let visited = Array(numNodes).fill(false);
+    let start = [startNode];
+    visited[startNode] = true;
+    newQueue(start,0);
+    getNodes(adjMatrix,visited);
+    while(getLastElement(prioqueue[0]) !== targetNode && prioqueue.length !== 0){
+        getNodes(adjMatrix,visited)
+    }
+    if(prioqueue.length === 0){
+        return [prioqueue,-1];
+    }else{
+        return [prioqueue,prioqueue[0].count]
+    }
+}
